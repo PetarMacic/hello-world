7112175fa923e52d0a17a1d2ea98d8335c94ee6e
PetarMacic <macic.petar@gmail.com>
2017-05-25 23:41:25 +0200

    Update index.php

--- index.php
+++ index.php
@@ -10,6 +10,7 @@ echo "A $color $fruit"; // A green apple
 <<<<<<< HEAD
 
 portable radi??????????
-=======
-pulllnem ti ga majke
+=====
+          radis mamu ti jebem 
+           nem ti ga majke
 >>>>>>> d123ce2a1626f77b056ebbed163d1605af199c39
